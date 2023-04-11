c6614af04082d112024964466ac42dad06566695
ZhaoChunshan <1900016633@pku.edu.cn>
2023-04-11 20:01:37 +0800

    COpy

---
+++ Value/Expression.h
@@ -0,0 +1,267 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include "../../PQuery/Varset.h"
+
+namespace GPStore{
+    
+class Expression;
+class AtomPropertyLabels;
+class Atom;
+
+/**
+ * 表达式类
+ * oprt!= EMPTY_OP，则内部结点，关注oprt和children
+ * oprt== EMPTY_OP，则叶子节点，关注atom（以及可能的property_label)
+ * 逻辑运算符OR XOR AND NOT组织成多叉树(e.g. 多个孩子用AND连起来)
+ * 其他的运算符组织成二叉树(i.e. children.size() == 2)
+*/
+class Expression{
+public:
+	enum OperatorType{
+        OR,
+        XOR, 
+        AND, 
+        NOT, 
+        EQUAL, NOT_EQUAL, LESS, GREATER, LESS_EQUAL, GREATER_EQUAL, 
+        IN,
+        STARTS_WITH, ENDS_WITH, CONTAINS,
+        IS_NULL, IS_NOT_NULL,
+        ADD, SUB,
+        MUL, DIV, MOD,
+        POW,
+        IDENTITY, NEGATION, // 正负号
+        INDEX, SLICE, // a[10], a["age"], or m[1:4]
+        // 最后一种是oC_PropertyOrLabelsExpression，存在propery_label里
+        EMPTY_OP    // no op, it's leaf node
+    };
+
+    OperatorType oprt_;
+	std::vector<Expression *> children_;	// children.size()==0 implies it's leaf node(i.e. atom)
+	Atom* atom_;
+    AtomPropertyLabels* property_label_;
+
+    // 变量覆盖集合
+    Varset covered_vars_;
+    // 解析完，执行中，给当前的WithBlock中的变量分配的ID。运行中变量的id，并不是sid oid eid这种。
+    std::vector<unsigned> covered_vars_id_; 
+    
+    Expression();
+    Expression(const Expression& that);
+    Expression& operator=(const Expression& that);
+    ~Expression();
+    void release();
+    
+    bool isAtom() const;
+
+    /* 将表达式树中所有的变量赋予变量的id，同时获得covered_vars_id_ */
+    void encode(const std::map<std::string, unsigned>& var2id);
+
+    void print(int dep) const;
+
+    
+    /*  helper functions */ 
+    static void printHead(int dep, const char *name, bool colon = true, bool endline = true);
+    static Atom * AtomDeepCopy(Atom *atom);
+    static std::string oprt2String(OperatorType op);
+};
+
+// 属性标签表达式，用于 p.props.age; a:Person 这类表达式
+class AtomPropertyLabels{
+public:
+    std::vector<std::string> key_names_;
+    std::vector<std::string> node_labels_;
+    AtomPropertyLabels();
+    AtomPropertyLabels(const AtomPropertyLabels &that);
+    AtomPropertyLabels& operator=(const AtomPropertyLabels &that);
+    void addKeyName(const std::string &k);
+    void addNodeLabel(const std::string &l);
+    void print(int dep) const;
+};
+
+class Atom{
+public:
+	enum AtomType{LITERAL, PARAMETER, CASE_EXPRESSION, COUNT, LIST_COMPREHENSION, PATTERN_COMPREHENSION,
+	QUANTIFIER, PATTERN_PREDICATE, FUNCTION_INVOCATION, EXISTENTIAL_SUBQUERY, VARIABLE};
+	AtomType atom_type_;
+    Varset covered_vars_;
+    std::vector<unsigned> covered_vars_id_;
+    Atom() = default;
+    virtual ~Atom() = 0;
+    virtual void encode(const std::map<std::string, unsigned>& var2id) = 0;
+    virtual void print(int dep)const = 0;
+};
+
+class Literal: public Atom{
+public:
+	enum LiteralType{BOOLEAN_LITERAL, NULL_LITERAL, INT_LITERAL, DOUBLE_LITERAL, STRING_LITERAL, LIST_LITERAL, MAP_LITERAL};
+	LiteralType literal_type;
+	double d;
+	long long i;
+	bool b;
+	std::string s;
+	std::vector<Expression *> list_literal;
+	std::map<std::string, Expression *> map_literal;
+    Literal();
+    Literal(LiteralType lt);
+    Literal(const Literal& that);
+
+    ~Literal();
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep)const override;
+};
+
+class Parameter: public Atom{
+public:
+	/* Cypher中的参数'$' ( oC_SymbolicName | DecimalInteger )，总是$开头，后面是字母串或者自然数*/
+	std::string symbolic_name;
+	unsigned  parameter_num;  // valid iff symbolic_name.length()==0
+    Parameter() ;
+    Parameter(unsigned n);
+    Parameter(const std::string &sym_name);
+    Parameter(const Parameter& that);
+
+    ~Parameter();
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+class CaseExpression: public Atom{
+public:
+	enum CaseType{SIMPLE, GENERIC};
+    CaseType case_type;
+	Expression *test_expr;  // not null if case_type == SIMPLE
+	std::vector<Expression *> when_exprs;
+	std::vector<Expression *> then_exprs;
+	Expression *else_expr;
+
+    CaseExpression();
+    CaseExpression(CaseType ct);
+    CaseExpression(const CaseExpression &that);
+    ~CaseExpression();
+
+    
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+/* only for aggregate func count(*) strange? */
+class Count : public Atom{
+public:
+    
+    Count();
+    Count(const Count& that);
+    ~Count();
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+/* TODO: We dont support ListComprehension in this version */
+class ListComprehension: public Atom{
+public:
+    /* [var in expression where filter | trans]*/
+	std::string var_name_;  // necessary
+    Expression *exp_;     // necessary
+    Expression *filter_;         // optional
+	Expression *trans_;          // optional
+
+    ListComprehension();
+    ListComprehension(const ListComprehension& that);
+    ~ListComprehension();
+
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+// TODO: We dont support pattern comprehension in this version.
+class PatternComprehension: public Atom{
+public:
+	std::string var_name_;	// optional
+	void* pattern_part_;         // necessary
+	Expression *filter_;         // optional
+	Expression *trans_;          // trans
+    PatternComprehension();
+    PatternComprehension(const PatternComprehension& that);
+    ~PatternComprehension();
+
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+// TODO: We dont support quantifier in this version.
+class Quantifier: public Atom{
+public:
+    /* all(variable IN list WHERE predicate) */
+	enum QuantifierType {ALL, ANY, SINGLE, NONE};
+	QuantifierType quantifier_type_;
+	std::string var_name_;
+    Expression *container_;
+    Expression *exp_;
+
+    Quantifier();
+    Quantifier(QuantifierType qt);
+    Quantifier(const Quantifier& that);
+    ~Quantifier();
+
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+// TODO: We dont support PatternPredicate in this version.
+class PatternPredicate: public Atom{
+public:
+	void *pattern_part;
+    PatternPredicate();
+    PatternPredicate(const PatternPredicate& that);
+    ~PatternPredicate();
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+// TODO: We dont support FunctionInvocation in this version.
+class FunctionInvocation : public Atom{
+public:
+	std::vector<std::string> func_name_;
+	bool distinct;	// for example, count(DISTINCT p.city)
+	std::vector<Expression *> args;
+
+    FunctionInvocation();
+    FunctionInvocation(const FunctionInvocation& that);
+    ~FunctionInvocation();
+
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+// TODO: We dont support ExistentialSubquery in this version.
+class ExistentialSubquery : public Atom{
+public:
+	//EXISTS SP? '{' SP? ( oC_RegularQuery | ( oC_Pattern ( SP? oC_Where )? ) ) SP? '}' ;
+	void *query_tree_;
+	void *pattern_;
+	Expression *where_;
+    ExistentialSubquery();
+    ExistentialSubquery(const ExistentialSubquery& that);
+    ~ExistentialSubquery();
+
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+class Variable: public Atom{
+public:
+    static const unsigned ID_NONE = 0xffffffffU;
+	std::string var_;
+    unsigned id_;
+    Variable();
+    Variable(const std::string &s);
+    Variable(const Variable& that);
+    ~Variable();
+
+    void encode(const std::map<std::string, unsigned>& var2id) override;
+    void print(int dep) const override;
+};
+
+
+}   // namespace GPStore
---
+++ Value/Value.h
@@ -0,0 +1,151 @@
+/**
+ * 所有的值都组织为Value。 一切赋值，构造都是深拷贝。
+ * 警告：
+ * (1)重载的'<'运算符，适用于order by的排序，和表达式中比较大小的'<'不是同一概念
+ * (2)重载的'=='运算符，适用于Distinct，Groupby，而不适用与表达式的相等‘=’
+ * (3) '<'号中，不支持Map之间的比较（Cypher规范未定义），但'=='支持Map之间的相等判断
+ * (4) order by排序，用<即可
+ * (5) Distinct，可以先排序再去重，但注意Map之间的去重，只能用暴力的(n^2)的方法，因为Map上没定义'<'关系。
+ * (6) hashCode函数暂时保留接口，不用！因为有些古怪。(Map, null, 以及浮点与整形相等...)
+*/
+
+#include <string>
+#include <map>
+#include <vector>
+
+namespace GPStore{
+typedef long long int_64;
+typedef unsigned long long uint_64;
+typedef unsigned char byte;
+
+class Value {
+public:
+    enum Type {
+        INTEGER, FLOAT, STRING, BOOLEAN,  // above && their array are storable
+        NODE, EDGE, PATH,
+        LIST, MAP,
+        NO_VALUE   // Cypher null
+    };
+    
+    enum EdgeType{
+        LEFT_EDGE, RIGHT_EDGE, UNDIRECTED
+    };
+
+    struct PathContent{
+        std::vector<EdgeType> edge_type_;
+        std::vector<uint_64> edge_id_;
+        std::vector<uint_64> node_id_;
+    };
+
+    union ValueUnion{
+        int_64 Int;
+        double Float;
+        std::string *String;
+        bool Boolean;
+
+        uint_64 Node;
+        uint_64 Edge;
+        PathContent *Path;
+        std::vector<Value *> *List;
+        std::map<std::string, Value *> *Map;
+    };
+
+    Type type_;
+    ValueUnion data_;
+
+    Value();
+    Value(Type _type_);
+    Value(const Value &other);
+    
+    Value(int_64 int_);
+    Value(double float_);
+    Value(const std::string& str);
+    Value(const char* s);
+    Value(bool b);
+    /* Construct Node or Edge */
+    Value(Type _type, uint_64 _id);
+    /* Construct Path */
+    Value(const PathContent &path_);
+    Value(const std::vector<uint_64>& node_id, const std::vector<uint_64> &edge_id, const std::vector<EdgeType>& edge_type);
+
+    /* Construct List */
+    Value(const std::vector<Value *> &list_);
+
+    /* Construct Map */
+    Value(const std::vector<std::string> &keys, const std::vector<Value *> &values);
+
+    ~Value();
+
+    bool isNull() const;
+    bool storable() const;
+
+    bool isIntArray() const;
+    bool isFloatArray() const;
+    bool isStringArray() const;
+    bool isBooleanArray() const;
+
+    uint_64 convertToBytes(void * ptr);
+
+    void constructFromBytes(const void * ptr, uint_64 n);
+
+    Type getType() const;
+    int_64 hashCode() const ;
+    bool operator==(const Value& other) const;    // check equivalence for DISTINCT
+    bool operator<(const Value& other) const;     // used for ORDER BY
+
+    Value& operator=(const Value&other);
+
+    
+    /* append an element to a list */
+    void append(const Value& value);
+
+    /* get a list element by index(must be valid) */
+    Value &operator[](uint_64 index);
+    
+    /* insert a (key, value) pair to a map */
+    void insert(const std::string &key, const Value &value);
+
+    /* find value by key, return nullptr if key doesnot exist */
+    Value *search(const std::string &key);
+
+    uint_64 size();
+    
+    std::string toString() const ;
+
+    static Value * ValueDeepCopy(const Value *value);
+
+private:
+    /* Construct Functions */
+    void ConstructFrom(const Value& other);
+
+    /* for order by */
+    bool CompareIntWith(const Value &other) const;
+    bool CompareFloatWith(const Value &other) const;
+    bool CompareStringWith(const Value &other) const;
+    bool CompareBooleanWith(const Value &other) const;
+    bool CompareNodeWith(const Value &other) const;
+    bool CompareEdgeWith(const Value &other) const;
+    bool ComparePathWith(const Value &other) const;
+    bool CompareListWith(const Value &other) const;
+    bool CompareMapWith(const Value &other) const;
+    bool CompareNoValueWith(const Value &other) const;
+
+    /* for distinct, group by */
+    bool IntEqualTo(const Value &other) const;
+    bool FloatEqualTo(const Value &other) const;
+    bool StringEqualTo(const Value &other) const;
+    bool BooleanEqualTo(const Value &other) const;
+    bool NodeEqualTo(const Value &other) const;
+    bool EdgeEqualTo(const Value &other) const;
+    bool PathEqualTo(const Value &other) const;
+    bool ListEqualTo(const Value &other) const;
+    bool MapEqualTo(const Value &other) const;
+    bool NoValueEqualTo(const Value &other) const;
+
+    /* destruct functions */
+    void Destruct();
+};
+
+
+/* helper functions */
+}
