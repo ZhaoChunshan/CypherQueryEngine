924b489f933bbcbdc35861f628a04b157703899e
ZhaoChunshan <1900016633@pku.edu.cn>
2023-04-12 17:25:49 +0800

    Rename File

--- PQuery/PQueryTree.h
+++ PQuery/PQueryTree.h
@@ -6,17 +6,18 @@
 namespace GPStore{
 
 
-class QueryTree{
+class PQueryTree{
 public:
     enum Mode {SPARQL, Cypher};
     Mode mode_;
     PQueryOperator *root_;
     /* Cypher global paramters */
     std::unordered_map<std::string, GPStore::Value> param_;
-
-    QueryTree();
-    QueryTree(Mode mode);
-    ~QueryTree();
+    /* SPARQL prefix name */
+    std::map<std::string, std::string> prefix_map;
+    PQueryTree();
+    PQueryTree(Mode mode);
+    ~PQueryTree();
 
     GPStore::Value setParam(const std::string& param_name, const GPStore::Value &val);
     GPStore::Value getParam(const std::string& param_name);
